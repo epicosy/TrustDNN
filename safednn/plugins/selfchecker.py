55e6979742589d6510366b312c9444a948924f87
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2024-04-06 16:57:23 -0700

    more command args

--- safednn/plugins/selfchecker.py
+++ safednn/plugins/selfchecker.py
@@ -14,29 +14,39 @@ class SelfChecker(ToolPlugin):
     class Meta:
         label = 'selfchecker'
 
-    def __init__(self, batch_size: int = 128, only_activation_layers: bool = False, **kw):
+    def __init__(self, batch_size: int = 128, only_dense_layers: bool = False, only_activation_layers: bool = False,
+                 **kw):
         super().__init__('selfchecker', **kw)
         self.batch_size = batch_size
         self.only_activation_layers = only_activation_layers
+        self.only_dense_layers = only_dense_layers
         self.has_metrics = True
 
     def analyze_command(self, model: Model, dataset: Dataset, working_dir: Path, **kwargs):
-        if not self.only_activation_layers:
-            command = f"-m {model.path} -wd {working_dir} -bs {self.batch_size} analyze "
-        else:
-            command = f"-m {model.path} -wd {working_dir} -bs {self.batch_size} -al analyze "
-        command += f"-tx {dataset.train.features_path} -ty {dataset.train.labels_path} "
+        command_args = f"-m {model.path} -wd {working_dir} -bs {self.batch_size} "
+
+        if self.only_dense_layers:
+            command_args += "-odl "
+
+        if self.only_activation_layers:
+            command_args += "-oal "
+
+        command = f"{command_args} analyze -tx {dataset.train.features_path} -ty {dataset.train.labels_path} "
         command += f"-vx {dataset.val.features_path} -vy {dataset.val.labels_path}"
         output = working_dir / 'pred_labels_valid.npy'
 
         return output, command
 
     def infer_command(self, model: Model, dataset: Dataset, working_dir: Path, **kwargs):
-        if not self.only_activation_layers:
-            subcommand = f"-m {model.path} -wd {working_dir} -bs {self.batch_size} infer "
-        else:
-            subcommand = f"-m {model.path} -wd {working_dir} -bs {self.batch_size} -al infer "
-        subcommand += f"-tx {dataset.test.features_path} -ty {dataset.test.labels_path}"
+        command_args = f"-m {model.path} -wd {working_dir} -bs {self.batch_size} "
+
+        if self.only_dense_layers:
+            command_args += "-odl "
+
+        if self.only_activation_layers:
+            command_args += "-oal "
+
+        subcommand = f"{command_args} infer -tx {dataset.test.features_path} -ty {dataset.test.labels_path}"
         output = working_dir / 'performance.json'
 
         return output, subcommand
