659074ed281f93020e7b2f5da0bc668e4c4f2b80
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2024-04-10 19:24:34 -0700

    added parameters to be dynamic

--- safednn/plugins/prophecy.py
+++ safednn/plugins/prophecy.py
@@ -10,18 +10,34 @@ class Prophecy(ToolPlugin):
     class Meta:
         label = 'prophecy'
 
-    def __init__(self, only_activation_layers: bool = False, **kw):
+    def __init__(self, only_dense_layers: bool = False, only_activation_layers: bool = False, skip_rules: bool = False,
+                 random_state: int = None, balance: bool = False, **kw):
         super().__init__('prophecy', **kw)
+        self.only_dense_layers = only_dense_layers
         self.only_activation_layers = only_activation_layers
+        self.skip_rules = skip_rules
+        self.random_state = random_state
+        self.balance = balance
 
     def analyze_command(self, model: Model, dataset: Dataset, working_dir: Path, **kwargs):
         command = f"-m {model.path} -wd {working_dir} analyze "
-        command += f"-tx {dataset.train.features_path} -ty {dataset.train.labels_path} "
+        command += (f"-tx {dataset.train.features_path} -ty {dataset.train.labels_path} "
+                    f"-vx {dataset.val.features_path} -vy {dataset.val.labels_path} ")
 
-        if not self.only_activation_layers:
-            command += f"-vx {dataset.val.features_path} -vy {dataset.val.labels_path} -odl -ial -sr"
-        else:
-            command += f"-vx {dataset.val.features_path} -vy {dataset.val.labels_path} -ial -sr"
+        if self.only_dense_layers:
+            command += "-odl "
+
+        if self.only_activation_layers:
+            command += "-oal "
+
+        if self.skip_rules:
+            command += "-sr "
+
+        if self.random_state:
+            command += f"-rs {self.random_state} "
+
+        if self.balance:
+            command += "-b "
 
         output = working_dir / 'ruleset.csv'
 
